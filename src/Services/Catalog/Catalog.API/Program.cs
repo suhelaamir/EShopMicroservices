1b15cfdba564a2c725ad3145a0c4cedfd9b6afbd
Mohammad Amir Suhel <suhelaamir68@gmail.com>
2025-02-07 08:25:57 +0530

    Program file

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,6 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//Add service to the container
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Configure the HTTP request pipeline
 
 app.Run();
