143bd67423ee67f4fee198af6f88605c3883c603
Mohammad Amir Suhel <suhelaamir68@gmail.com>
2025-02-08 08:05:34 +0530

    Added Carter Mapster and for Minimal API endpoints

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,5 +1,25 @@
 ﻿namespace Catalog.API.Products.CreateProduct;
 
-public class CreateProductEndpoint
+//This will call thru minimal API, Minimal API will invoke by Carter Library
+
+public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+public record CreateProductResponse(Guid Id);
+
+public class CreateProductEndpoint : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        //Expose minimal endpoints from here
+        app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<CreateProductResponse>();
+                return Results.Created($"/product/{response.Id}", response);
+            }).
+            WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .WithSummary("Create Product")
+            .WithDescription("Create Product");
+    }
 }
